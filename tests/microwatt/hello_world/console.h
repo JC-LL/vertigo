9c34306526ed045ddb9df3d67c2202e665c7418a
jc-ll <jc.lelann@gmail.com>
2020-04-03 18:53:44 +0200

    Remove unneeded files coming with microwatt

--- tests/microwatt/hello_world/console.h
+++
@@ -1,5 +0,0 @@
-void potato_uart_init(void);
-int getchar(void);
-void putchar(unsigned char c);
-void putstr(const char *str, unsigned long len);
-size_t strlen(const char *s);
--- tests/microwatt/scripts/gen_icache_tb.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-b = bytearray()
-for i in range(0x100):
-    b = b + i.to_bytes(4, 'little')
-f = open('icache_test.bin', 'w+b')
-f.write(b)
-f.close()
-
