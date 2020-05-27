ab265051f66f3d1b936e5913fc8ff67b9d8a201f
jc-ll <jc.lelann@gmail.com>
2020-05-27 17:29:57 +0200

    get rid of ERB

--- lib/vertigo/tb_generator.rb
+++ lib/vertigo/tb_generator.rb
@@ -1,5 +1,3 @@
-require 'erb'
-
 module Vertigo
 
   class TestBenchGenerator
@@ -13,14 +11,129 @@ module Vertigo
 
     def generate_from ast
       @ast=ast
-      entity_arch=find_entity_arch
-      detecting_clk_and_reset entity_arch
-      @tb_name=entity_arch.first.name.str+'_tb'
-      erb=ERB.new(IO.read "#{__dir__}/template.tb.vhd")
-      vhdl_tb=erb.result(binding)
+      entity_arch=find_entity_arch()
+      detecting_clk_and_reset(entity_arch)
+      vhdl_tb=gen_code()
+      @tb_name=@entity_name+"_tb"
       tb_filename=@tb_name+".vhd"
-      File.open(tb_filename,'w'){|f| f.puts vhdl_tb}
+      File.open(tb_filename,'w'){|f| f.puts vhdl_tb.finalize}
       puts "=> generated testbench : #{tb_filename}"
+    end
+
+    def line n=80
+      "-"*n
+    end
+
+    def comment str
+      "-- #{str}"
+    end
+
+    def gen_code
+      code=Code.new
+      code << gen_header
+      code << "library ieee;"
+      code << "use ieee.std_logic_1164.all;"
+      code << "use ieee.numeric_std.all;"
+      code.newline
+      code << "entity #{@entity_name}_tb is"
+      code << "end entity;"
+      code.newline
+      code << "architecture bhv of #{@entity_name}_tb is"
+      code.indent=2
+      code << "constant HALF_PERIOD : time :=5 ns;"
+      code.newline
+      code << "signal #{@clk_name} : std_logic := '0';"
+      code << "signal #{@reset_name} : std_logic := '0';"
+      code.newline
+      code << "signal running : boolean := true;"
+      code.newline
+      code << "procedure wait_cycles(n : natural) is "
+      code << "begin"
+      code.indent=4
+      code << "for i in 0 to n loop"
+      code.indent=6
+      code << "wait until rising_edge(#{@clk_name});"
+      code.indent=4
+      code << "end loop;"
+      code.indent=2
+      code << "end procedure;"
+      @entity.ports.each do |port|
+        port_name=port.name.str.ljust(@max_length_str)
+        port_type=port.type.str
+        code << "signal #{port_name} : #{port_type};" unless @excluded.include?(port)
+      end
+      code.indent=0
+      code << "begin"
+      code.indent=2
+      code << gen_clock_and_reset
+      code << instanciate_dut
+      code << gen_stim_process
+      code.indent=0
+      code << "end bhv;"
+      code
+    end
+
+    def gen_header
+      code=Code.new
+      code << line
+      code << "-- this file was generated automatically by Vertigo Ruby utility"
+      code << "-- date : (d/m/y h:m) #{Time.now.strftime("%d/%m/%Y %k:%M")}"
+      code << "-- author : Jean-Christophe Le Lann - 2014"
+      code << line
+      code.newline
+      code
+    end
+
+    def gen_clock_and_reset
+      code=Code.new
+      code << line
+      code << comment("clock and reset")
+      code << line
+      code << "#{@reset_name} <= '0','1' after 666 ns;"
+      code.newline
+      code << "#{@clk_name} <= not(#{@clk_name}) after HALF_PERIOD when running else #{@clk_name};"
+      code
+    end
+
+    def instanciate_dut
+      code=Code.new
+      code << line
+      code << comment("Design Under Test")
+      code << line
+      code << "dut : entity work.#{@entity_name}(#{@arch_name})"
+      code.indent=2
+      code << "port map ("
+      code.indent=4
+
+      @entity.ports.each do |port|
+        port_name=port.name.str.ljust(@max_length_str)
+        port_type=port.type.str
+        code << "#{port_name} => #{port_name},"
+      end
+      code.indent=2
+      code << ");"
+      code.indent=0
+      code
+    end
+
+    def gen_stim_process
+      code=Code.new
+      code << line
+      code << comment("sequential stimuli")
+      code << line
+      code << "stim : process"
+      code << "begin"
+      code.indent=2
+      code << "report \"running testbench for #{@entity_name}(#{@arch_name})\";"
+      code << "report \"waiting for asynchronous reset\";"
+      code << "wait until #{@reset_name}='1';"
+      code << "wait_cycles(100);"
+      code << "report \"end of simulation\";"
+      code << "running <= false;"
+      code << "wait;"
+      code.indent=0
+      code << "end process;"
+      code
     end
 
     private
@@ -38,6 +151,8 @@ module Vertigo
       end
 
       puts "=> found architecture '#{arch.name.str}'"
+      @entity_name=@entity.name.str
+      @arch_name=@arch.name.str
       [@entity,@arch]
     end
 
@@ -51,6 +166,8 @@ module Vertigo
       puts "\t-most probable reset : #{@rst.name.str}"
       @max_length_str=entity.ports.map{|port| port.name.str.size}.max
       @excluded=[@clk,@rst]
+      @reset_name=@rst.name.str
+      @clk_name=@clk.name.str
     end
 
     def levenshtein_distance(s, t)
